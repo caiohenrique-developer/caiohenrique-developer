e46d05e777f13d558f0c466c2b1dd6038b92c2d9
caiohenrique-developer <caiorussellrox@msn.com>
2021-10-04 13:55:34 -0300

    🔨 [chore]: Configuration to use emoji with commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,15 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
-
+  
     /*
     * Any rules defined here will override rules from @commitlint/config-conventional
     */
     rules: {
-        'type-empty': [0, 'never'],
-        'subject-empty': [0, 'never'],
-        'body-max-line-length': [0, 'never'],
-        'type-case': [2, 'always', ['upper-case', 'lower-case']],
-        'scope-case': [2, 'always',
+        'type-empty': [ 0, 'never' ],
+        'subject-empty': [ 0, 'never' ],
+        'body-max-line-length': [ 0, 'never' ],
+        'type-case': [ 2, 'always', ['upper-case','lower-case'] ],
+        'scope-case': [ 2, 'always',
             [
                 'lower-case', // default
                 'upper-case', // UPPERCASE
@@ -21,7 +21,7 @@ module.exports = {
                 'start-case' // Start Case
             ]
         ],
-        'subject-case': [2, 'always',
+        'subject-case': [ 2, 'always',
             [
                 'lower-case', // default
                 'upper-case', // UPPERCASE
@@ -33,7 +33,7 @@ module.exports = {
                 'start-case' // Start Case
             ]
         ],
-        'body-case': [2, 'always',
+        'body-case': [ 2, 'always',
             [
                 'lower-case', // default
                 'upper-case', // UPPERCASE
@@ -45,13 +45,13 @@ module.exports = {
                 'start-case' // Start Case
             ]
         ],
-        'type-enum': [2, 'always',
+        'type-enum': [ 2, 'always',
             [
                 'feat',
                 'fix',
                 'perf',
                 'chore',
-
+  
                 'FEAT',
                 'FIX',
                 'DOC',
@@ -62,9 +62,10 @@ module.exports = {
                 'TEST',
                 'REVERT',
                 'WIP',
-
-                ':sparkles: [FEAT]'
+  
+                '🚨 [Test]'
             ]
         ],
     },
-}
+  }
+  
